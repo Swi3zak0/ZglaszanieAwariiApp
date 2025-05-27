4efaed7112347c2954f7ca53d154fd63672ab2ba
Swi3zak0 <swiezak.gd@wp.pl>
2025-05-27 23:32:44 +0200

    Initial commit

---
+++ Forms/MainForm.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace ZglaszanieAwariiApp.Forms
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnZglosAwarie = new Button();
+            btnListaAwarii = new Button();
+            btnSprawdzStatus = new Button();
+            SuspendLayout();
+            // 
+            // btnZglosAwarie
+            // 
+            btnZglosAwarie.Location = new Point(620, 77);
+            btnZglosAwarie.Name = "btnZglosAwarie";
+            btnZglosAwarie.Size = new Size(131, 23);
+            btnZglosAwarie.TabIndex = 0;
+            btnZglosAwarie.Text = "Zgłoś awarię";
+            btnZglosAwarie.UseVisualStyleBackColor = true;
+            btnZglosAwarie.Click += btnZglosAwarie_Click;
+            // 
+            // btnListaAwarii
+            // 
+            btnListaAwarii.Location = new Point(620, 178);
+            btnListaAwarii.Name = "btnListaAwarii";
+            btnListaAwarii.Size = new Size(131, 23);
+            btnListaAwarii.TabIndex = 1;
+            btnListaAwarii.Text = "Wszystkie Awarie";
+            btnListaAwarii.UseVisualStyleBackColor = true;
+            btnListaAwarii.Click += btnListaAwarii_Click;
+            // 
+            // btnSprawdzStatus
+            // 
+            btnSprawdzStatus.Location = new Point(620, 282);
+            btnSprawdzStatus.Name = "btnSprawdzStatus";
+            btnSprawdzStatus.Size = new Size(131, 23);
+            btnSprawdzStatus.TabIndex = 2;
+            btnSprawdzStatus.Text = "Sprawdź Status";
+            btnSprawdzStatus.UseVisualStyleBackColor = true;
+            btnSprawdzStatus.Click += btnSprawdzStatus_Click;
+            // 
+            // MainForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(btnSprawdzStatus);
+            Controls.Add(btnListaAwarii);
+            Controls.Add(btnZglosAwarie);
+            Name = "MainForm";
+            Text = "MainForm";
+            Load += MainForm_Load;
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button btnZglosAwarie;
+        private Button btnListaAwarii;
+        private Button btnSprawdzStatus;
+    }
+}
---
+++ Forms/MainForm.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ZglaszanieAwariiApp.Forms
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnZglosAwarie_Click(object sender, EventArgs e)
+        {
+            {
+                new FormZgloszenie().ShowDialog();
+            }
+        }
+
+        private void btnListaAwarii_Click(object sender, EventArgs e)
+        {
+            {
+                new FormZgloszenie().ShowDialog();
+            }
+        }
+
+        private void btnSprawdzStatus_Click(object sender, EventArgs e)
+        {
+            {
+                new FormZgloszenie().ShowDialog();
+            }
+        }
+    }
+}
